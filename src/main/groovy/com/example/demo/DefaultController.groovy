3b2bd2faef78603d1c6842baff51603393c8d274
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2022-01-27 15:19:48 +0100

    updating demo to hit dev.g2f.fr
    Not working yet

--- src/main/groovy/com/example/demo/DefaultController.groovy
+++ src/main/groovy/com/example/demo/DefaultController.groovy
@@ -1,13 +1,33 @@
 package com.example.demo
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient
+import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class DefaultController {
 
-    @RequestMapping(value="/aaa",produces="text/plain;charset=utf-8")
-    public String defaultController() {
-        "coucou chat !"
+  @GetMapping("/index")
+  public String index(Model model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+                      @AuthenticationPrincipal OAuth2User oauth2User) {
+      String name = oauth2User.getName()
+      String clientName = authorizedClient.getClientRegistration().getClientName()
+      Map<String, Object> attributes = oauth2User.getAttributes()
+
+      model.addAttribute("userName", name)
+      model.addAttribute("clientName", clientName)
+      model.addAttribute("userAttributes", attributes)
+
+      "index";
+  }
+
+    @GetMapping("/login")
+    public String login()
+    {
+        "login"
     }
 }
